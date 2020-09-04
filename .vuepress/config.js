1df5ea251c0077d32461e7f7ca684e76b1737798
Star (Shun) Li <starlee.li@fatritech.com>
2020-09-04 11:31:42 +0800

    window 查看端口 pid 终止 pid

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -66,7 +66,8 @@ module.exports = {
       ],
       '/server/': [
         '',
-        'server-link'
+        'server-link',
+        'window-kill-port'
       ],
       '/javascript/': [
         'this'
